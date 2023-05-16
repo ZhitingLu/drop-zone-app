8404995d685e79e3b37ca2cf29b28208943787a0
ZhitingLu <luzhiting@outlook.com>
2023-05-16 21:24:30 +0200

    Added dropzone preview

--- public/script.js
+++ public/script.js
@@ -1,37 +1,49 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const dropzone = document.getElementById("dropzone");
-    const fileInput = document.getElementById("file-input");
-    const fileList = document.getElementById("file-list");
-  
-    dropzone.addEventListener("dragover", (event) => {
-      event.preventDefault();
-      dropzone.classList.add("highlight");
-    });
-  
-    dropzone.addEventListener("dragleave", () => {
-      dropzone.classList.remove("highlight");
-    });
-  
-    dropzone.addEventListener("drop", (event) => {
-      event.preventDefault();
-      dropzone.classList.remove("highlight");
-  
-      const files = Array.from(event.dataTransfer.files);
-      showFiles(files);
-    });
-  
-    fileInput.addEventListener("change", () => {
-      const files = Array.from(fileInput.files);
-      showFiles(files);
-    });
-  
-    function showFiles(files) {
-      fileList.innerHTML = "";
-      files.forEach((file) => {
-        const listItem = document.createElement("li");
-        listItem.textContent = file.name;
-        fileList.appendChild(listItem);
-      });
+const dropzone = document.getElementById('dropzone');
+const fileList = document.getElementById('file-list');
+
+dropzone.addEventListener('dragover', (e) => {
+  e.preventDefault();
+  dropzone.classList.add('dragover');
+});
+
+dropzone.addEventListener('dragleave', () => {
+  dropzone.classList.remove('dragover');
+});
+
+dropzone.addEventListener('drop', (e) => {
+  e.preventDefault();
+  dropzone.classList.remove('dragover');
+
+  const files = e.dataTransfer.files;
+  showFilePreviews(files);
+});
+
+fileInput.addEventListener('change', (e) => {
+  const files = e.target.files;
+  showFilePreviews(files);
+});
+
+function showFilePreviews(files) {
+  fileList.innerHTML = '';
+
+  for (const file of files) {
+    const listItem = document.createElement('li');
+    listItem.textContent = file.name;
+
+    if (file.type.includes('image/')) {
+      const img = document.createElement('img');
+      img.src = URL.createObjectURL(file);
+      img.alt = file.name;
+      listItem.appendChild(img);
+    } else if (file.type.includes('video/')) {
+      const video = document.createElement('video');
+      video.src = URL.createObjectURL(file);
+      video.autoplay = true;
+      video.loop = true;
+      video.muted = true;
+      listItem.appendChild(video);
     }
-  });
-  
+
+    fileList.appendChild(listItem);
+  }
+}
--- src/components/DropZone.js
+++ src/components/DropZone.js
@@ -4,17 +4,35 @@ import { useDropzone } from 'react-dropzone';
 const DropZone = () => {
   const { getRootProps, getInputProps, acceptedFiles } = useDropzone();
 
+const DropZone = () => {
+  const { getRootProps, getInputProps, acceptedFiles } = useDropzone();
+
   return (
     <div {...getRootProps()} className="dropzone">
       <input {...getInputProps()} />
       <p>Drag and drop files here, or click to select files</p>
       <ul>
         {acceptedFiles.map((file) => (
-          <li key={file.name}>{file.name}</li>
+          <li key={file.name}>
+            {file.type.includes('image') && (
+              <img src={URL.createObjectURL(file)} alt={file.name} />
+            )}
+            {file.type.includes('video') && (
+              <video controls>
+                <source src={URL.createObjectURL(file)} type={file.type} />
+              </video>
+            )}
+            {!file.type.includes('image') && !file.type.includes('video') && (
+              <span>{file.name}</span>
+            )}
+          </li>
         ))}
       </ul>
     </div>
   );
 };
+
+export default DropZone;
+
 
 export default DropZone;
